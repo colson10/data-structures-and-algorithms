99ca4ff6c9a8a7eed7a8300bc6125e72d6e414a7
Carl Olson <colson10@gmail.com>
2018-05-31 09:20:58 -0700

    updated traversal name

--- tree-intersection/tree-intersection.js
+++ tree-intersection/tree-intersection.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const _inOrderTraversal = (node, map, array, callback) => { // eslint-disable-line
+const _preOrderTraversal = (node, map, array, callback) => { // eslint-disable-line
   if (!node) return undefined;
   callback(map, node.value, array);
 
-  _inOrderTraversal(node.left, map, array, callback);
-  _inOrderTraversal(node.right, map, array, callback);
+  _preOrderTraversal(node.left, map, array, callback);
+  _preOrderTraversal(node.right, map, array, callback);
   return undefined;
 };
 
@@ -26,40 +26,7 @@ export default (tree1, tree2) => {
   const map = new Map();
   const results = [];
 
-  _inOrderTraversal(tree1.root, map, null, _mapValues);
-  _inOrderTraversal(tree2.root, map, results, _pushValues);
+  _preOrderTraversal(tree1.root, map, null, _mapValues);
+  _preOrderTraversal(tree2.root, map, results, _pushValues);
   return results;
 };
-
-// const inOrderTraversalMap = (node, map) => {
-//   if (!node) return undefined;
-//   map[node.value] = 1;
-
-//   inOrderTraversalMap(node.left, map);
-//   inOrderTraversalMap(node.right, map);
-//   return undefined;
-// };
-
-// const resultsInOrderTraversal = (node, map, results) => {
-//   if (!node) return undefined;
-//   if (map[node.value]) {
-//     results.push(node.value);
-//   }
-//   resultsInOrderTraversal(node.left, map, results);
-//   resultsInOrderTraversal(node.right, map, results);
-//   return undefined;
-// };
-
-// const treeIntersection = (tree1, tree2) => {
-//   if (!tree1 || !tree2) {
-//     return null;
-//   }
-//   const map = new Map();
-//   const results = [];
-
-//   inOrderTraversalMap(tree1.root, map);
-//   resultsInOrderTraversal(tree2.root, map, results);
-//   return results;
-// };
-
-// export default treeIntersection;
